7f5f9db11f669e42593e707a5d2fe80e0ef8323b
Peter Juras <p.juras@outlook.com>
2016-05-25 00:34:41 +0200

    Initial Android project skeleton

---
+++ app/src/main/java/pma/com/nutrifami/StartActivity.java
@@ -0,0 +1,13 @@
+package pma.com.nutrifami;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_start);
+        }
+}
