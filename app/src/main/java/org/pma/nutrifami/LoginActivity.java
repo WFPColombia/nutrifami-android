99086f8a7f7fb8b491e9bdbc4c1f8b91dc06426c
Peter Juras <p.juras@outlook.com>
2016-05-30 22:31:19 +0200

    Add login page

---
+++ app/src/main/java/org/pma/nutrifami/LoginActivity.java
@@ -0,0 +1,213 @@
+package org.pma.nutrifami;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+    private UserLoginTask mAuthTask = null;
+
+    // UI references.
+    private EditText mPassportTextView;
+    private EditText mVoucherIdTextView;
+    private View mProgressView;
+    private View mLoginFormView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Set up the login form.
+        mPassportTextView = (EditText) findViewById(R.id.passport_id);
+
+        mVoucherIdTextView = (EditText) findViewById(R.id.voucher_id);
+        mVoucherIdTextView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button signInButton = (Button) findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+    }
+
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid passport id, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        mPassportTextView.setError(null);
+        mVoucherIdTextView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String passport = mPassportTextView.getText().toString();
+        String voucherId = mVoucherIdTextView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid voucherId, if the user entered one.
+        if (!TextUtils.isEmpty(voucherId) && !isVoucherIdValid(voucherId)) {
+            mVoucherIdTextView.setError(getString(R.string.error_invalid_voucher_id));
+            focusView = mVoucherIdTextView;
+            cancel = true;
+        }
+
+        // Check for a valid passport id.
+        if (TextUtils.isEmpty(passport)) {
+            mPassportTextView.setError(getString(R.string.error_field_required));
+            focusView = mPassportTextView;
+            cancel = true;
+        } else if (!isPassportIdValid(passport)) {
+            mPassportTextView.setError(getString(R.string.error_invalid_passport));
+            focusView = mPassportTextView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true);
+            mAuthTask = new UserLoginTask(passport, voucherId);
+            mAuthTask.execute((Void) null);
+        }
+    }
+
+    private boolean isPassportIdValid(String passport) {
+        return true;
+    }
+
+    private boolean isVoucherIdValid(String voucherId) {
+        return true;
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mPassportId;
+        private final String mVoucherId;
+
+        UserLoginTask(String passportId, String voucherId) {
+            mPassportId = passportId;
+            mVoucherId = voucherId;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+
+            try {
+                // Simulate network access.
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+            showProgress(false);
+
+            if (success) {
+                finish();
+            } else {
+                mVoucherIdTextView.setError(getString(R.string.error_login));
+                mVoucherIdTextView.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+            showProgress(false);
+        }
+    }
+}
+
--- app/src/main/java/org/pma/nutrifami/StartActivity.java
+++ app/src/main/java/org/pma/nutrifami/StartActivity.java
@@ -1,7 +1,10 @@
 package org.pma.nutrifami;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Button getStartedButton = (Button) findViewById(R.id.get_started_button);
+        final StartActivity activity = this;
+        getStartedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
