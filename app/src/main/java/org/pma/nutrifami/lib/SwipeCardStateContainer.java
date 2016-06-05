8892812eccb45a69a4e14ee251b2517a73e8b29f
Peter Juras <p.juras@outlook.com>
2016-06-05 23:55:04 +0200

    Change view reference to weak reference

--- app/src/main/java/org/pma/nutrifami/lib/SwipeCardStateContainer.java
+++ app/src/main/java/org/pma/nutrifami/lib/SwipeCardStateContainer.java
@@ -10,22 +10,25 @@ import org.pma.nutrifami.adapter.SwipeCardData;
 import org.pma.nutrifami.adapter.SwipeCardType;
 import org.pma.nutrifami.model.Unit;
 
+import java.lang.ref.WeakReference;
+
 /**
  * Created by Peter on 05.06.2016.
  */
 public class SwipeCardStateContainer {
-    private View mRootCardView;
+    private WeakReference<View> mRootCardView;
     private SwipeCardData mSwipeCardData;
 
     public SwipeCardStateContainer(View rootCardView, SwipeCardData swipeCardData) {
-        this.mRootCardView = rootCardView;
+        this.mRootCardView = new WeakReference<>(rootCardView);
         this.mSwipeCardData = swipeCardData;
     }
 
     public void updateView() {
+        View rootCardView = this.mRootCardView.get();
         if (this.mSwipeCardData.getCardType() == SwipeCardType.Explanation) {
-            Animation fadeInAnimation = AnimationUtils.loadAnimation(mRootCardView.getContext(), R.anim.fadein);
-            TextView textView = (TextView) mRootCardView.findViewById(R.id.swipe_card_text);
+            Animation fadeInAnimation = AnimationUtils.loadAnimation(rootCardView.getContext(), R.anim.fadein);
+            TextView textView = (TextView) rootCardView.findViewById(R.id.swipe_card_text);
 
             textView.setVisibility(View.VISIBLE);
             textView.startAnimation(fadeInAnimation);
